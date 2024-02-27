dc73bab8637956b40617fc534937191cc418f642
MattPereira <mattpereira.dev@gmail.com>
2024-02-27 11:40:00 -0700

    deploy contracts on mumbai

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useCallback, useRef, useState } from "react";
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { BanknotesIcon, Bars3Icon, BugAntIcon, HeartIcon } from "@heroicons/react/24/outline";
@@ -99,12 +99,10 @@ export const Header = () => {
           )}
         </div>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
-          <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
-          </div>
+          <div className="flex relative w-10 h-10 text-3xl">🍄</div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">FundGuys</span>
+            <span className="text-xs">Public Goods Platform</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
