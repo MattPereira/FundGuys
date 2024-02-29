49ce2093577695e3ac2291265aba5646e5c720d1
Sam Wellander <samwellander@gmail.com>
2024-02-29 13:50:45 -0700

    add campaign data

--- packages/nextjs/app/campaigns/page.tsx
+++ packages/nextjs/app/campaigns/page.tsx
@@ -1,8 +1,17 @@
+"use client";
+
 // import Link from "next/link";
 import type { NextPage } from "next";
+import { CampaignCard } from "~~/components/fundguys/CampgainCard";
 import { CreateCampaign } from "~~/components/fundguys/CreateCampaign";
+import { useScaffoldEventHistory } from "~~/hooks/scaffold-eth";
 
 const Campaigns: NextPage = () => {
+  const { data, isLoading } = useScaffoldEventHistory({
+    contractName: "PublicGoodsFunding",
+    eventName: "ProjectCreated",
+    fromBlock: 0n, // can be set to the block in which we deploy on Base
+  });
   return (
     <>
       <div className="px-5 sm:px-7 md:px-20 my-10">
@@ -15,9 +24,17 @@ const Campaigns: NextPage = () => {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {Array.from(Array(6).keys()).map((_, idx) => (
-            <div key={idx} className="skeleton animate-pulse bg-base-100 rounded-xl w-full h-72"></div>
-          ))}
+          {isLoading &&
+            Array.from(Array(6).keys()).map((_, idx) => (
+              <div key={idx} className="skeleton animate-pulse bg-base-100 rounded-xl w-full h-72"></div>
+            ))}
+          {data?.map(({ args: { projectAddress } }) =>
+            projectAddress ? (
+              <CampaignCard key={projectAddress} contractAddress={projectAddress} />
+            ) : (
+              <div key={projectAddress} />
+            ),
+          )}
         </div>
       </div>
     </>
