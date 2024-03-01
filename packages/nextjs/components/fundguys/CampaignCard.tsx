4908f9bedf8e81a55632499745088594e86e027b
Sam Wellander <samwellander@gmail.com>
2024-02-29 18:17:06 -0700

    add donate button to campaign card

--- packages/nextjs/components/fundguys/CampaignCard.tsx
+++ packages/nextjs/components/fundguys/CampaignCard.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+import { DonateModal } from "./DonateModal";
 import { Address } from "viem";
 import { formatUnits } from "viem";
 import { useContractRead } from "wagmi";
@@ -10,21 +12,20 @@ interface Props {
 }
 
 export const CampaignCard = ({ contractAddress }: Props) => {
-  const {
-    data: projectData,
-    isLoading,
-    isError,
-  } = useContractRead({
+  const [donateModalOpen, setDonateModalOpen] = useState(false);
+
+  const { data: projectData = {}, isLoading } = useContractRead({
     address: contractAddress,
     abi: ProjectABI,
     functionName: "getProject",
   });
 
-  if (isLoading) return <div className="skeleton animate-pulse bg-base-100 rounded-xl w-full h-72"></div>;
-  if (isError || contractAddress === undefined) return <h1>Failed to fetch Campaign data</h1>;
-  const [title, description, image, targetAmount, amountRaised, deadline, completed] = projectData as any;
+  const [title, description, image, projectTokenAddress, targetAmount, amountRaised, deadline, completed] =
+    projectData as any;
 
-  const percentageBigInt = (amountRaised * BigInt(100)) / targetAmount;
+  if (isLoading) return <div className="skeleton animate-pulse bg-base-100 rounded-xl w-full h-72"></div>;
+
+  const percentageBigInt = (amountRaised * BigInt(100)) / (targetAmount || 1n);
   const percentage = Number(percentageBigInt);
   return (
     <div className="bg-base-200 rounded-xl">
@@ -40,7 +41,15 @@ export const CampaignCard = ({ contractAddress }: Props) => {
         </div>
         <p>{deadline}</p>
         <p>{completed}</p>
+        <button onClick={() => setDonateModalOpen(true)} className="donate">
+          Donate
+        </button>
       </div>
+      <DonateModal
+        projectTokenAddress={projectTokenAddress}
+        open={donateModalOpen}
+        setClosed={() => setDonateModalOpen(false)}
+      />
     </div>
   );
 };
